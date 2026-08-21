b2fa515d92536bc3d7e726c9b10c8ab80a582225
gitbackerZ <rodiyuju57@gmail.com>
2026-08-21 11:37:42 +0800

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.jonjon.pokeapi_champions"
+    namespace = "com.example.pokemon_mb_companion"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -16,7 +16,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.jonjon.pokeapi_champions"
+        applicationId = "com.example.pokemon_mb_companion"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
